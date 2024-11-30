a7b37f6d987119e4249a775530d3ab77b7ebda25
AKASH KM <akashunni869@gmail.com>
2024-11-30 11:02:05 +0530

    test object

--- twoPointers/valid_palindrome.py
+++ twoPointers/valid_palindrome.py
@@ -4,4 +4,7 @@ class Solution:
         for i in s:
            if i.isalnum():
             newStr+=i.lower()
+        print(newStr)
         return newStr == newStr[::-1]
+obj=Solution()
+print(obj.isPalindrome('aka'))
