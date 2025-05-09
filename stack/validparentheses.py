7db8fa5e76b6c10bb4fd17ce9c0b32b1d1dc9fea
AkashMuraliNEW <akashmurali013@gmail.com>
2025-05-09 22:04:11 +0530

    blind 75

--- stack/validparentheses.py
+++ stack/validparentheses.py
@@ -2,4 +2,9 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         parenDict ={ '}' : '{', ')':'(', ']':'['}
-     
+        for c in s:
+            if c in parenDict:
+                if stack and stack[-1] == parenDict[c]:
+                    stack.pop() 
+                else:
+                    return False
