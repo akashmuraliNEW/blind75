826365414a35d444347653d1abc2c91e4361748a
AkashMuraliNEW <akashmurali013@gmail.com>
2025-05-11 21:35:00 +0530

    removed

--- arrays&hash/valid_Anagram.py
+++ arrays&hash/valid_Anagram.py
@@ -1,9 +1,9 @@
-# class Solution:
-#     def isAnagram(self, s: str, t: str) -> bool:
-#         if len(s) != len(t):
-#             return False
-#         countS, countT = {}, {}
-#         for i in range(len(s)):
-#             countS[s[i]] = 1 + countS.get(s[i], 0) #get function used to give default value 
-#             countT[t[i]] = 1 + countT.get(t[i], 0)
-#         return countS == countT     
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0) #get function used to give default value 
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT     
