4f1c6142730c6a83fdd29a9ca0cd286e1acac3fb
AkashMuraliNEW <akashmurali013@gmail.com>
2025-05-25 13:37:13 +0530

    Blind75 prblems

--- arrays&hash/GroupAnagrams.py
+++ arrays&hash/GroupAnagrams.py
@@ -1,12 +1,10 @@
 from collections import defaultdict
-
-
-strs = ["act","pots","tops","cat","stop","hat"]
-dict1 = defaultdict(list)
-for s in strs:
-    count = [0] * 26
-    for c in s:
-        count[ord(c)-ord("a")]+=1
-    # print(count)
-    dict1[tuple(count)].append(s)
-print( dict1.values())
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        resultDict = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c)-ord('a')]+=1
+            resultDict[tuple(count)].append(s)
+        return resultDict.values()
